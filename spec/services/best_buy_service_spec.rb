033c7b7c8e2cf2d98c6b4fbc43ea98b95ca51e83
Joe Perry <joseph.w.perry@gmail.com>
2016-03-08 11:43:02 -0700

    Search spec passing

--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -11,8 +11,13 @@ describe "BestBuyService" do
       VCR.use_cassette("best_buy_service#products") do
         products_json = @best_buy_service.products("sennheiser")
         products = products_json[:products]
-        binding.pry
         expect(products.count).to eq(15)
+        expect(products.first[:sku]).to eq(4763330)
+        expect(products.first[:name]).to eq("Galaxy Audio - Headset Microphone - Beige")
+        expect(products.first[:customerReviewAverage]).to eq(nil)
+        expect(products.first[:shortDescription][0..10]).to eq("GALAXY AUDI")
+        expect(products.first[:salePrice]).to eq(199.99)
+        expect(products.first[:image]).to eq("http://images.bestbuy.com/BestBuy_US/images/products/4763/4763330_sa.jpg")
       end
     end
   end
